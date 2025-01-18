54c93203779c5a460b5bd570698385b4dd578cd6
piyush0528 <tiwaripiyush262@gmail.com>
2025-01-18 15:35:24 +0530

    added

---
+++ practiceproblem.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class practiceproblem{
+public static void main(String args[]){
+
+for(int i=1; i<=10; i++){
+if(i%2!=0){
+System.out.println(i+" ");}
+
+}
+for(int i=1; i<=10; i++){
+if(i%2==0){
+System.out.println(i+" ");}
+
+}
+}
+}
---
+++ problem1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class problem1{
+public static void main(String[] args){
+Scanner sc= new Scanner(System.in);
+int num= sc.nextInt();
+if(num == 0)
+System.out.println("ZERO");
+if(num == 1)
+System.out.println("ONE");
+if(num == 2)
+System.out.println("TWO");
+if(num == 3)
+System.out.println("THREE");
+if(num == 4)
+System.out.println("FOUR");
+if(num == 5)
+System.out.println("FIVE");
+if(num == 6)
+System.out.println("SIX");
+if(num == 7)
+System.out.println("SEVEN");
+if(num == 8)
+System.out.println("EIGHT");
+if(num == 9)
+System.out.println("NINE");
+}
+}
